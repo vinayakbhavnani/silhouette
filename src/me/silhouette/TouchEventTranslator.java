63fd34e3f88789ea3c6076fc18761993f9f4b6bb
Vinayak Bhavnani <vinayak.bhavnani@gmail.com>
2013-06-26 17:10:56 +0530

    event translator which translated touch events into draw events

---
+++ src/me/silhouette/TouchEventTranslator.java
@@ -0,0 +1,40 @@
+package me.silhouette;
+
+import android.graphics.Path;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class TouchEventTranslator implements View.OnTouchListener
+{
+    Path mPath;
+    public TouchEventTranslator(View view , Path path)
+    {
+        mPath = path;
+    }
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent)
+    {
+        int action = motionEvent.getAction();
+        if (action == MotionEvent.ACTION_DOWN)
+        {
+            Log.d("doodle","action down"+action);
+            mPath.moveTo(motionEvent.getX(),motionEvent.getY());
+        }
+        else if(action == MotionEvent.ACTION_UP)
+        {
+            Log.d("doodle","action up"+action);
+        }
+        else if(action == MotionEvent.ACTION_MOVE)
+        {
+            Log.d("doodle","action move"+action);
+            mPath.lineTo(motionEvent.getX(),motionEvent.getY());
+        }
+        else
+        {
+            Log.d("doodle","action other"+action);
+        }
+        view.invalidate();
+        return true;
+    }
+}
